67565436affd1060f6382d09db2626ef5cd800cf
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-24 21:03:14 +0700

    Update komplainPemilik.php

--- pemilik/komplainPemilik.php
+++ pemilik/komplainPemilik.php
@@ -6,30 +6,30 @@ require 'sidebar3.php';
     <div class="container-fluid">
       <div class="container">
         <div class="row justify-content-center">
-          <div class="col-md-6 text-center mb-5">
+          <div class="col-md-6 text-center mb-5 mt-3">
             <h2 class="heading-section">Kritik Saran</h2>
           </div>
         </div>
 
-        <div class="card">
+        <div class="card" style=" border-top: 0; border-left: 0; border-right: 0; border-bottom: 3px solid grey;">
           <div class="card-body">
-            <h5 class="card-title">Selasa 10 September 2021</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <h5 class="card-title" style="color:#5C8E9E">Selasa 10 September 2021</h5>
+            <p class="card-text">Maaf pak sebelumnya, tapi saya mau saran kalau bisa ruang kumpul dan dapur diberi beberapa aturan supaya ruangannya tetap bersih. Terimakasih.</p>
           </div>
         </div>
 
-        <div class="card text">
+        <div class="card" style=" border-top: 0; border-left: 0; border-right: 0; border-bottom: 3px solid grey;">
           <div class="card-body">
-            <h5 class="card-title">Selasa 10 September 2021</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <img class= "https://www.google.com/search?q=couple+silhouette+tumblr&tbm=isch&hl=id&sa=X&ved=2ahUKEwik_faR1an0AhVsuksFHT9EAocQrNwCKAB6BQgBEO4B&biw=1263&bih=609#imgrc=TN7dG4G0lc-EvM" alt="">
+            <h5 class="card-title" style="color:#5C8E9E">Selasa 10 September 2021</h5>
+            <p class="card-text">Pak saya samalem liat si anu bawa cewe kekamar, setau saya si anu gak punya sodara cewe, tapi nggak tau juga pak, coba tanyain.</p>
           </div>
+          <img src="komplain.svg" alt="komplain" style="width:280; height:180;">  
         </div>
 
-        <div class="card text">
+        <div class="card" style=" border-top: 0; border-left: 0; border-right: 0; border-bottom: 3px solid grey;">
           <div class="card-body">
-            <h5 class="card-title">Selasa 10 September 2021</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <h5 class="card-title" style="color:#5C8E9E">Selasa 10 September 2021</h5>
+            <p class="card-text">Pak semalem jam 11 an di ruang kumpul berisik banget, kayaknya si anu bawa temen-temennya kesini tapi gadir ngobrol mulu ampe jam 1 an. Tolong dibilangin pak ato gak kasih peraturan di ruang kumpul</p>
           </div>
         </div>
       </div>
@@ -38,8 +38,6 @@ require 'sidebar3.php';
 <style>
     .content-container {
     padding-top: 50px;
-    }
-    .content-container {
     padding-left: 220px;
     }
 </style>
