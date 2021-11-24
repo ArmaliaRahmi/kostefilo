3866e0a5277ea932ab462cb24a49634bae0a2015
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-24 21:02:12 +0700

    Update tagihanPemilik.php

--- pemilik/tagihanPemilik.php
+++ pemilik/tagihanPemilik.php
@@ -7,13 +7,13 @@ require 'sidebar3.php';
         <section class="ftco-section">
             <div class="container">
                 <div class="row justify-content-center">
-                    <div class="text-center mb-5 mt-5">
+                    <div class="col-md-6 text-center mb-5 mt-3">
                         <h2 class="heading-section">TAGIHAN</h2>
                     </div>
                 </div>
                     <div class="col-md-12">
                         <div class="table-responsive">
-                        <table class="table table-bordered">
+                        <table class="contact-table table table-curved">
                             <thead class="text-center">
                             <tr>
                                 <th rowspan="2" style="vertical-align : middle;text-align:center;">Kmr</th>
@@ -40,157 +40,180 @@ require 'sidebar3.php';
                             
                             </thead>
                             <tbody class="text-center">
-                                <style>
-                                    table td:nth-child(4) {
-                                    background: #5C8E9E;
-                                    }
-                                    table td:nth-child(6) {
-                                    background: #5C8E9E;
-                                    }
-                                    table td:nth-child(8) {
-                                    background: #5C8E9E;
-                                    }
-                                    table td:nth-child(10) {
-                                    background: #5C8E9E;
-                                    }
-                                    table td:nth-child(12) {
-                                    background: #5C8E9E;
-                                    }
-                                    table td:nth-child(14) {
-                                    background: #5C8E9E;
-                                    }
-                                </style>
                                 <tr>
-                                    <td class="text-left number">1</td>
-                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td>1</td>
+                                    <td>Colombia</td>
                                     <td></td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></i</td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
+                                    <td></td>
                                     <td></td>
                                 </tr>
                                 <tr>
-                                    <td class="text-left number">1</td>
-                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td>1</td>
+                                    <td>Colombia</td>
                                     <td></td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></i</td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
                                     <td></td>
-                                </tr>
-                                <tr>
-                                <td class="text-left number">1</td>
-                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
                                     <td></td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
+                                    <td></td>
                                     <td></td>
                                 </tr>
                                 <tr>
-                                <td class="text-left number">1</td>
-                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td>1</td>
+                                    <td>Colombia</td>
                                     <td></td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></i</td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
                                     <td></td>
-                                </tr>
-                                <tr>
-                                    <td class="text-left number">1</td>
-                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
                                     <td></td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
+                                    <td></td>
                                     <td></td>
                                 </tr>
                                 <tr>
-                                    <td class="text-left number">1</td>
-                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td>1</td>
+                                    <td>Colombia</td>
                                     <td></td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
-                                    <td>26</td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></i</td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Colombia</td>
+                                    <td></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></i</td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Colombia</td>
+                                    <td></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></i</td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td><i class='fas fa-check' style='font-size:24px; color: #505050'></td>
+                                    <td></td>
                                     <td></td>
                                 </tr>
                             </tbody>
                         </table>
                         <style>
-                            table.table-bordered {
-                            border: 2px solid red;
+                            .table-curved {
+                            border-collapse: separate;
+                            border: solid black 2px;
                             border-radius: 30px;
-                            overflow: hidden;
+                            border-left: 0px;
+                            border-top: 0px;
                             }
-                            table.table-bordered > thead > tr > th{
+                            table.table-curved > thead > tr > th{
                             border:2px solid black;
                             }
-
-                            table.table-bordered > tbody > tr > td{
+                            table.table-curved > tbody > tr > td{
                                 border:2px solid black;
                             }
-                        </style>
+                            .table-curved > thead:first-child > tr:first-child > th {
+                                border-bottom: 2px;
+                                border-top: solid black 2px;
+                            }
+                            .table-curved td, .table-curved th {
+                                border-left: 2px solid black;
+                                border-top: 2px solid black;
+                            }
+                            .table-curved > :first-child > :first-child > :first-child {
+                                border-top-left-radius: 30px;
+                            }
+                            .table-curved > :first-child > :first-child > :last-child {
+                                border-top-right-radius: 30px;
+                            }
+                            .table-curved > :last-child > :last-child > :first-child {
+                                border-bottom-left-radius: 30px;
+                            }
+                            .table-curved > :last-child > :last-child > :last-child {
+                                border-bottom-right-radius: 30px;
+                            }
+                            table td:nth-child(4) {
+                            background: #5C8E9E;
+                            }
+                            table td:nth-child(6) {
+                            background: #5C8E9E;
+                            }
+                            table td:nth-child(8) {
+                            background: #5C8E9E;
+                            }
+                            table td:nth-child(10) {
+                            background: #5C8E9E;
+                            }
+                            table td:nth-child(12) {
+                            background: #5C8E9E;
+                            }
+                            table td:nth-child(14) {
+                            background: #5C8E9E;
+                            }
+                            .content-container {
+                            padding-top: 50px;
+                            padding-left: 220px;
+                            }
+                            </style>
+                        
                     </div>
                 </div>
             </div>
         </section>
     </div>
 </div>
-<meta name='viewport' content='width=device-width, initial-scale=1'>
 <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
