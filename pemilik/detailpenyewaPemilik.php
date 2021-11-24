597ea49d0ac1a6a1b7fd0325acd71ccd0806544f
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-24 08:11:33 +0700

    Create detailpenyewaPemilik.php

---
+++ pemilik/detailpenyewaPemilik.php
@@ -0,0 +1,67 @@
+<?php
+require 'sidebar3.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+     <style>
+    .content-container {
+    padding-top: 50px;
+    }
+    .content-container {
+    padding-left: 220px;
+    }
+
+     </style>
+   </head> 
+
+    <body>
+    <div class="content-container">
+        <div class="container-fluid">
+            <div class="container">
+            <div class="row justify-content-start">
+                <div class="col-sm-6">
+                    <div class="card border-0">
+                        <div class="card-body">
+                            <h6 class="card-subtitle mt-4 mb-2" style= "color:#5C8E9E">Nama Lengkap</h6>
+                            <p class="card-text">Data Science</p>
+                            <h6 class="card-subtitle mt-3 mb-2" style= "color:#5C8E9E">TTL</h6>
+                            <p class="card-text">PT. Telkom Indonesia</p>
+                            <h6 class="card-subtitle mt-3 mb-2" style= "color:#5C8E9E">No. Telp</h6>
+                            <p class="card-text">Full Time</p>
+                            <h6 class="card-subtitle mt-3 mb-2" style= "color:#5C8E9E">Asal</h6>
+                            <p class="card-text">(+62) 8123-456-251</p>
+                            <h6 class="card-subtitle mt-4 mb-2" style= "color:#5C8E9E">Alamat</h6>
+                            <p class="card-text">Siti Rahma</p>
+                            <h6 class="card-subtitle mt-3 mb-2" style= "color:#5C8E9E">Kontak Keluarga / Relasi</h6>
+                            <p class="card-text">sitirahma@gmail.com</p>
+                            <h6 class="card-subtitle mt-3 mb-2" style= "color:#5C8E9E">Lembaga</h6>
+                            <p class="card-text">(+62) 8123-456-251</p>
+                            <h6 class="card-subtitle mt-3 mb-2" style= "color:#5C8E9E">NO. Plat Kendaraan</h6>
+                            <p class="card-text">(+62) 8123-456-251</p>
+                            <h6 class="card-subtitle mt-3 mb-2" style= "color:#5C8E9E">Jenis Kendaraan</h6>
+                            <p class="card-text">(+62) 8123-456-251</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6" >
+                    <div class="card border-0">
+                        <div class="card-body">
+                            <h6 class="card-subtitle mt-5 mb-2 text-muted">Foto KTP</h6>
+                            <p class="card-text">Bandung, Indonesia</p>
+                            <h6 class="card-subtitle mt-3 mb-2 text-muted">foto KK</h6>
+                            <p class="card-text">PT. Telkom Indonesia</p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+        </div>    
+            </div>
+        </div>
+    </body>
+
+</html>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
