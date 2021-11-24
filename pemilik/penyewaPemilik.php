902951f7e9fb4af3f29327b88ca7755f5da9e8d8
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-24 21:02:46 +0700

    Update penyewaPemilik.php

--- pemilik/penyewaPemilik.php
+++ pemilik/penyewaPemilik.php
@@ -7,73 +7,107 @@ require 'sidebar3.php';
         <section class="ftco-section">
             <div class="container">
                 <div class="row justify-content-center">
-                    <div class="col-md-5 text-center mb-5 mt-5">
+                    <div class="col-md-6 text-center mb-5 mt-3">
                         <h2 class="heading-section">PENYEWA</h2>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-md-12">
-                        <div class="table-wrap">
-                        <table class="table table-bordered">
-                            <thead class="point-table-head">
+                <div class="col-md-12">
+                        <div class="table-responsive">
+                        <table class="contact-table table table-curved">
+                            <thead class="text-center">
                                 <tr>
                                     <th class="text-center">Kmr</th>
                                     <th class="text-center">Nama Lengkap</th>
+                                    <th class="text-center">Asal</th>
                                     <th class="text-center">WA</th>
                                     <th class="text-center">Lembaga</th>
                                 </tr>
                             </thead>
                             <tbody class="text-center">
                                 <tr>
-                                    <td class="text-left number">1</td>
-                                    <td class="text-left"><span>Colombia</span> </td>
-                                    <td>38</td>
-                                    <td>26</td>
+                                    <td>01</td>
+                                    <td style="color #5C8E9E">Akis Aga Punta Dewa</td>
+                                    <td>Bogor</td>
+                                    <td>0812-9876-4322</td>
+                                    <td>UGM - Teknik Pertanian</td>
                                 </tr>
                                 <tr>
-                                    <td class="text-left number">1</td>
-                                    <td class="text-left"><span>Colombia</span> </td>
-                                    <td>38</td>
-                                    <td>26</td>
+                                    <td>02</td>
+                                    <td style="color #5C8E9E">Anggito Satya</td>
+                                    <td>Solo</td>
+                                    <td>0812-3445-2222</td>
+                                    <td>UGM - Teknik Mesin</td>
                                 </tr>
                                 <tr>
-                                    <td class="text-left number">1</td>
-                                    <td class="text-left"><span>Colombia</span> </td>
-                                    <td>38</td>
-                                    <td>26</td>
+                                    <td>03</td>
+                                    <td style="color #5C8E9E"></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
                                 </tr>
                                 <tr>
-                                    <td class="text-left number">1</td>
-                                    <td class="text-left"><span>Colombia</span> </td>
-                                    <td>38</td>
-                                    <td>26</td>
+                                    <td>04</td>
+                                    <td style="color #5C8E9E">Riyansyah Ramdha</td>
+                                    <td>Bandung</td>
+                                    <td>0878-3949-0898</td>
+                                    <td>UNY - Teknik Elektro</td>
                                 </tr>
                                 <tr>
-                                    <td class="text-center number">1</td>
-                                    <td class="text-cemter"><span>Colombia</span> </td>
-                                    <td>38</td>
-                                    <td>26</td>
+                                    <td>05</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
                                 </tr>
                                 <tr>
-                                    <td class="text-center number">1</td>
-                                    <td class="text-center"><span>Colombia</span> </td>
-                                    <td>38</td>
-                                    <td>26</td>
+                                    <td>06</td>
+                                    <td style="color #5C8E9E">Riyansyah Ramdha</td>
+                                    <td>Bandung</td>
+                                    <td>0878-3949-0898</td>
+                                    <td>UNY - Teknik Elektro</td>
+                                </tr>
                                 </tr>
                                 </div>
                             </tbody>
                         </table>
                         <style>
-                            table.table-bordered {
-                            border-collapse: collapse;
+                             .table-curved {
+                            border-collapse: separate;
+                            border: solid black 2px;
                             border-radius: 30px;
-                            overflow: hidden;
+                            border-left: 0px;
+                            border-top: 0px;
                             }
-                            table.table-bordered > thead > tr > th{
+                            table.table-curved > thead > tr > th{
                             border:2px solid black;
                             }
-                            table.table-bordered > tbody > tr > td{
+                            table.table-curved > tbody > tr > td{
                                 border:2px solid black;
+                            }
+                            .table-curved > thead:first-child > tr:first-child > th {
+                                border-bottom: 2px;
+                                border-top: solid black 2px;
+                            }
+                            .table-curved td, .table-curved th {
+                                border-left: 2px solid black;
+                                border-top: 2px solid black;
+                            }
+                            .table-curved > :first-child > :first-child > :first-child {
+                                border-top-left-radius: 30px;
+                            }
+                            .table-curved > :first-child > :first-child > :last-child {
+                                border-top-right-radius: 30px;
+                            }
+                            .table-curved > :last-child > :last-child > :first-child {
+                                border-bottom-left-radius: 30px;
+                            }
+                            .table-curved > :last-child > :last-child > :last-child {
+                                border-bottom-right-radius: 30px;
+                            }
+                            .content-container {
+                            padding-top: 50px;
+                            padding-left: 220px;
                             }
                         </style>
                     </div>
