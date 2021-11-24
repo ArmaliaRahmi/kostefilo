24058f3d49a18e558bf9b1986de41f188f4da54c
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-24 09:31:43 +0700

    Update login.php

--- login.php
+++ login.php
@@ -4,30 +4,31 @@
     rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" 
     crossorigin="anonymous">
 <body>
-<div class="main">
-    <div style="padding : 100px;" >
-      <div align="center">
-        <h3 class=primary>SIGN IN</h3>
-        
-        <div class=container >
-            <form  method="post" action="">
-                <div class="mb-3">
-                    <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Username" style="width:401px; height: 47px; left:514px; top:323px"id="email">
-                </div>
-                <div class="mb-3">
-                    <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Password" style="width:401px; height: 47px; left:514px; top:323px"id="email">
-                </div>
-
-                <div class="button" style="margin-bottom: 0px" >
-                <button type="submit" class="btn btn-outline-dark" name ="login" style="background-color:white; width:401px; height: 47px; left:514px; bottom:20px;"><span style="color:black;">LOGIN</span></button>
-                </div>
-            </form>
-      </div>
-    </div> 
-</div>
+<div
+  class="bg-image d-flex justify-content-center align-items-center"
+  style="
+    background-image: url('bglogin.svg');
+    height: 100vh;
+  ">
+    <div class="main">
+        <div style="padding : 100px;" >
+        <div align="center">
+            <h2 class=primary style="color: white">SIGN IN</h2>
+            
+            <div class=container >
+                <form  method="post" action="">
+                    <div class="mt-5 mb-2">
+                        <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Username" style="width:401px; height: 47px; left:514px; top:323px"id="email">
+                    </div>
+                    <div class="mb-2">
+                        <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Password" style="width:401px; height: 47px; left:514px; top:323px"id="email">
+                    </div>
+                    <div class="button mt-5 py-5" >
+                    <button type="submit" class="btn btn-outline-dark" name ="login" style="background-color:white; width:401px; height: 47px; left:514px; bottom:20px;"><span style="color:black;">LOGIN</span></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        </div> 
+    </div>
 </body>
-<style>
-    body{
-        background-img: url('kostefilo/bglogin.svg');
-    }
-</style> 
