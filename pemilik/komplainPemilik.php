d5324aa5b13e0a82c18dc66ee8c12dd64713aef2
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-24 08:10:48 +0700

    Create komplainPemilik.php

---
+++ pemilik/komplainPemilik.php
@@ -0,0 +1,49 @@
+<?php
+require 'sidebar3.php';
+?>
+
+<div class="content-container">
+    <div class="container-fluid">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-6 text-center mb-5">
+            <h2 class="heading-section">Kritik Saran</h2>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Selasa 10 September 2021</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+
+        <div class="card text">
+          <div class="card-body">
+            <h5 class="card-title">Selasa 10 September 2021</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <img class= "https://www.google.com/search?q=couple+silhouette+tumblr&tbm=isch&hl=id&sa=X&ved=2ahUKEwik_faR1an0AhVsuksFHT9EAocQrNwCKAB6BQgBEO4B&biw=1263&bih=609#imgrc=TN7dG4G0lc-EvM" alt="">
+          </div>
+        </div>
+
+        <div class="card text">
+          <div class="card-body">
+            <h5 class="card-title">Selasa 10 September 2021</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+<style>
+    .content-container {
+    padding-top: 50px;
+    }
+    .content-container {
+    padding-left: 220px;
+    }
+</style>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
