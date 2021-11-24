206e128f7026f15afb06121eafa25651856fc37f
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-24 08:09:45 +0700

    Create berandaPemilik.php

---
+++ pemilik/berandaPemilik.php
@@ -0,0 +1,132 @@
+<?php
+require 'sidebar3.php';
+?>
+
+<div class="content-container">
+    <div class="container-fluid">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-6 text-center mb-5">
+            <h2 class="heading-section">Info Kamar</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="me-2">
+              <div class="card" >
+                <div class="card-body">
+                  <h5 class="card-title text-center">F 110</h5>
+                  <div class="wrapper" align="center">
+                  <span class="border card-title text-center">06</span>
+                  <span class="border card-title text-center">06</span>
+                  <span class="border card-title text-center">06</span>
+                  <span class="border card-title text-center">06</span>
+                  <span class="border card-title text-center">06</span>
+                  </div>
+                  <br>
+                  <div class="container" align="center">
+                  <span class="border card-title text-center">06</span>
+                  <span class="border card-title text-center">06</span>
+                  <span class="border card-title text-center">06</span>
+                  <span class="border card-title text-center">06</span>
+                  <span class="border card-title text-center">06</span>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-6">
+          <div class="card" >
+            <div class="card-body">
+              <h5 class="card-title text-center">F 29</h5>
+              <div class="container" align="center">
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+              </div>
+              <br>
+              <div class="container" align="center">
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+              </div>
+              <br>
+              <div class="container" align="center">
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-6 margin-5">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title text-center">F 25</h5>
+              <div class="card">
+              <div class="container" align="center">
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+              </div>
+              </div>
+              <br>
+              <div class="card-group">
+              <div class="card">
+              <div class="container" align="center">
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+                <span class="border card-title text-center">06</span>
+              </div>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-6 mb-5">
+          <div class="card-group">
+            <div class="card-body">
+              <span class="border"></span>
+              <br>
+              <span class="border"></span>
+              <br>
+              <span class="border"></span>
+              <br>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>    
+  </div>
+</div>
+
+<style>
+  .border {
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    margin: -2px;
+  }
+  .content-container {
+  padding-top: 50px;
+  }
+  .content-container {
+  padding-left: 220px;
+  }
+</style>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
