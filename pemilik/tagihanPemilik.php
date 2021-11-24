6b13280c52a0b3446bc7961686dc31bf6f78c919
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-25 00:26:26 +0700

    Update tagihanPemilik.php

--- pemilik/tagihanPemilik.php
+++ pemilik/tagihanPemilik.php
@@ -214,8 +214,3 @@ require 'sidebar3.php';
         </section>
     </div>
 </div>
-<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
