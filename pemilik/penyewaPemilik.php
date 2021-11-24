ada39eb44f232ebe8ebbaf57f97adf052ebb18ad
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-25 00:25:00 +0700

    Update penyewaPemilik.php

--- pemilik/penyewaPemilik.php
+++ pemilik/penyewaPemilik.php
@@ -116,7 +116,3 @@ require 'sidebar3.php';
         </section>
     </div>
 </div>
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
