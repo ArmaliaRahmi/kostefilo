f55c96be6de881f5a92289af804a835f64829b9e
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-24 08:11:57 +0700

    Create penyewaPemilik.php

---
+++ pemilik/penyewaPemilik.php
@@ -0,0 +1,88 @@
+<?php
+require 'sidebar3.php';
+?>
+
+<div class="content-container">
+    <div class="container-fluid">
+        <section class="ftco-section">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-5 text-center mb-5 mt-5">
+                        <h2 class="heading-section">PENYEWA</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="table-wrap">
+                        <table class="table table-bordered">
+                            <thead class="point-table-head">
+                                <tr>
+                                    <th class="text-center">Kmr</th>
+                                    <th class="text-center">Nama Lengkap</th>
+                                    <th class="text-center">WA</th>
+                                    <th class="text-center">Lembaga</th>
+                                </tr>
+                            </thead>
+                            <tbody class="text-center">
+                                <tr>
+                                    <td class="text-left number">1</td>
+                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td>38</td>
+                                    <td>26</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-left number">1</td>
+                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td>38</td>
+                                    <td>26</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-left number">1</td>
+                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td>38</td>
+                                    <td>26</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-left number">1</td>
+                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td>38</td>
+                                    <td>26</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-center number">1</td>
+                                    <td class="text-cemter"><span>Colombia</span> </td>
+                                    <td>38</td>
+                                    <td>26</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-center number">1</td>
+                                    <td class="text-center"><span>Colombia</span> </td>
+                                    <td>38</td>
+                                    <td>26</td>
+                                </tr>
+                                </div>
+                            </tbody>
+                        </table>
+                        <style>
+                            table.table-bordered {
+                            border-collapse: collapse;
+                            border-radius: 30px;
+                            overflow: hidden;
+                            }
+                            table.table-bordered > thead > tr > th{
+                            border:2px solid black;
+                            }
+                            table.table-bordered > tbody > tr > td{
+                                border:2px solid black;
+                            }
+                        </style>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
