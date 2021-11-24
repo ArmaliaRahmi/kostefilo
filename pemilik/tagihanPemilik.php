2266ca6fcc0d42dc22a6154c7d902d8d80fa09c0
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-24 08:10:17 +0700

    Create tagihanPemilik.php

---
+++ pemilik/tagihanPemilik.php
@@ -0,0 +1,176 @@
+<?php
+require 'sidebar3.php';
+?>
+
+<div class="content-container">
+    <div class="container-fluid">
+        <section class="ftco-section">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="text-center mb-5 mt-5">
+                        <h2 class="heading-section">TAGIHAN</h2>
+                    </div>
+                </div>
+                    <div class="col-md-12">
+                        <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead class="text-center">
+
+                            <tr>
+                                <th rowspan="2" style="vertical-align : middle;text-align:center;">Kmr</th>
+                                <th rowspan="2" style="vertical-align : middle;text-align:center;">Nama</th>
+                                <th rowspan="2" style="vertical-align : middle;text-align:center; "><i style='font-size:24px' class='fas'>	&#xf536;</i></th>
+                                <th colspan="12" class="text-center">2021</th>
+                                <th rowspan="2" style="vertical-align : middle;text-align:center;"><i style='font-size:24px' class='fas'>&#xf531;</i></th>
+                            </tr>
+                            <tr>
+                            <th class="text-center">Jan</th>
+                            <th class="text-center">Feb</th>
+                            <th class="text-center">Mar</th>
+                            <th class="text-center">Apr</th>
+                            <th class="text-center">Mei</th>
+                            <th class="text-center">Jun</th>
+                            <th class="text-center">Jul</th>
+                            <th class="text-center">Agu</th>
+                            <th class="text-center">sep</th>
+                            <th class="text-center">Okt</th>
+                            <th class="text-center">Nov</th>
+                            <th class="text-center">Des</th>
+                            </tr>
+                            </thead>
+                            <tbody class="text-center">
+                                <tr>
+                                    <td class="text-left number">1</td>
+                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td></td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td class="text-left number">1</td>
+                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td></td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                <td class="text-left number">1</td>
+                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td></td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                <td class="text-left number">1</td>
+                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td></td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td class="text-left number">1</td>
+                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td></td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td class="text-left number">1</td>
+                                    <td class="text-left"><span>Colombia</span> </td>
+                                    <td></td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td>26</td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <style>
+                            table {
+                            border-collapse: collapse;
+                            border-radius: 30px;
+                            overflow: hidden;
+                            }
+                            table.table-bordered > thead > tr > th{
+                            border:2px solid black;
+                            }
+                            table.table-bordered > tbody > tr > td{
+                                border:2px solid black;
+                            }
+                        </style>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
