4f92a7faecba2070187229205b12c976041c3835
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-25 00:24:00 +0700

    Update berandaPemilik.php

--- pemilik/berandaPemilik.php
+++ pemilik/berandaPemilik.php
@@ -6,104 +6,137 @@ require 'sidebar3.php';
     <div class="container-fluid">
       <div class="container">
         <div class="row justify-content-center">
-          <div class="col-md-6 text-center mb-5">
+          <div class="col-md-6 text-center mb-5 mt-3">
             <h2 class="heading-section">Info Kamar</h2>
           </div>
         </div>
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="me-2">
-              <div class="card" >
+        <div class="card-columns">
+              <div class="card text-center" style="width:400px; left:50px; border:2px solid black; border-radius:30px;">
                 <div class="card-body">
                   <h5 class="card-title text-center">F 110</h5>
-                  <div class="wrapper" align="center">
-                  <span class="border card-title text-center">06</span>
-                  <span class="border card-title text-center">06</span>
-                  <span class="border card-title text-center">06</span>
-                  <span class="border card-title text-center">06</span>
-                  <span class="border card-title text-center">06</span>
-                  </div>
+                  <div class="card border-light">
+                    <div class="container">
+                    <div class="card" style="width:200px; height:90px;">
+                          <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">06</span>
+                          <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">07</span>
+                          <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">08</span>
+                          <span class="border border-dark card-title text-center">09</span>
+                          <span class="border border-dark card-title text-center">-</span>
+                          <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">01</span>
+                          <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">02</span>
+                          <span class="border border-dark card-title text-center">03</span>
+                          <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">04</span>
+                          <span class="border border-dark card-title text-center">05</span>
+                      </div>
+                    </div> 
+                  </div> 
+                </div>
+              </div>
+              <div class="card text-center" style="width:400px; left:200px; border:2px solid black; border-radius:30px;">
+                <div class="card-body">
+                  <h5 class="card-title text-center">F 29</h5>
+                  <div class="card border-light">
+                  <div class="container">
+                  <div class="card" style="width:200px; height:90px;">
+                        <span class="border border-dark card-title text-center">220</span>
+                        <span class="border border-dark card-title text-center">221</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">222</span>
+                        <span class="border border-dark card-title text-center">223</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">224</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">210</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">211</span>
+                        <span class="border border-dark card-title text-center">212</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">213</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #DE3B3B">214</span>
+                    </div>
+                  </div> 
+                  </div> 
                   <br>
-                  <div class="container" align="center">
-                  <span class="border card-title text-center">06</span>
-                  <span class="border card-title text-center">06</span>
-                  <span class="border card-title text-center">06</span>
-                  <span class="border card-title text-center">06</span>
-                  <span class="border card-title text-center">06</span>
-                  </div>
+                  <div class="card border-light">
+                  <div class="container">
+                  <div class="card" style="width:200px; height:90px;">
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">225</span>
+                        <span class="border border-dark card-title text-center">226</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">227</span>
+                        <span class="border border-dark card-title text-center">228</span>
+                        <span class="border border-dark card-title text-center">229</span>
+                        <span class="border border-dark card-title text-center">215</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">216</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">217</span>
+                        <span class="border border-dark card-title text-center">218</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">219</span>
+                    </div>
+                  </div> 
+                  </div> 
+                  <br>
+                  <div class="card border-light">
+                  <div class="container">
+                  <div class="card" style="width:200px; height:90px;">
+                        <span class="border border-dark card-title text-center">205</span>
+                        <span class="border border-dark card-title text-center">206</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">207</span>
+                        <span class="border border-dark card-title text-center">208</span>
+                        <span class="border border-dark card-title text-center">209</span>
+                        <span class="border border-dark card-title text-center">200</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">201</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">202</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">203</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">204</span>
+                    </div>
+                  </div> 
+                </div> 
               </div>
             </div>
           </div>
-        </div>
 
-        <div class="col-sm-6">
-          <div class="card" >
-            <div class="card-body">
-              <h5 class="card-title text-center">F 29</h5>
-              <div class="container" align="center">
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-              </div>
+          <div class="card-columns">
+              <div class="card text-center" style="width:400px; left:50px; border:2px solid black; border-radius:30px;">
+                <div class="card-body">
+                  <h5 class="card-title text-center">F 25</h5>
+                  <div class="card border-light">
+                  <div class="container">
+                    <div class="card" style="width:200px; height:90px;">
+                        <span class="border border-dark card-title text-center">120</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #DE3B3B">121</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">122</span>
+                        <span class="border border-dark card-title text-center">123</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">124</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">110</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">111</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">112</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">113</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">114</span>
+                    </div>
+                  </div> 
+                </div> 
               <br>
-              <div class="container" align="center">
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-              </div>
-              <br>
-              <div class="container" align="center">
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-              </div>
-            </div>
+              <div class="card border-light">
+                  <div class="container">
+                    <div class="card" style="width:200px; height:90px;">
+                        <span class="border border-dark card-title text-center">125</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">126</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">127</span>
+                        <span class="border border-dark card-title text-center">128</span>
+                        <span class="border border-dark card-title text-center">129</span>
+                        <span class="border border-dark card-title text-center">115</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">116</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #5C8E9E;">117</span>
+                        <span class="border border-dark card-title text-center">118</span>
+                        <span class="border border-dark card-title text-center" style="background-color: #DE3B3B">119</span>
+                    </div>
+                  </div> 
+                </div> 
           </div>
         </div>
 
-        <div class="col-6 margin-5">
-          <div class="card">
+        <div class="card border-light" style="width:250px; left:200px;">
+                <div class="card-body">
             <div class="card-body">
-              <h5 class="card-title text-center">F 25</h5>
-              <div class="card">
-              <div class="container" align="center">
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-              </div>
-              </div>
+              <p ><span class="border border-dark" style="float:left; background-color: #5C8E9E"></span> = Isi</p>
               <br>
-              <div class="card-group">
-              <div class="card">
-              <div class="container" align="center">
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-                <span class="border card-title text-center">06</span>
-              </div>
-              </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-6 mb-5">
-          <div class="card-group">
-            <div class="card-body">
-              <span class="border"></span>
+              <p><span class="border border-dark" style="float:left;"></span> = Kosong</p>
               <br>
-              <span class="border"></span>
-              <br>
-              <span class="border"></span>
+              <p><span class="border border-dark" style="float:left; background-color: #DE3B3B"></span> = Segera Kosong</p>
               <br>
             </div>
           </div>
@@ -118,15 +151,11 @@ require 'sidebar3.php';
     display: inline-block;
     width: 40px;
     height: 40px;
-    margin: -2px;
+    margin:-2px;
+    border: 10px solid black;
   }
   .content-container {
   padding-top: 50px;
-  }
-  .content-container {
   padding-left: 220px;
   }
 </style>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
