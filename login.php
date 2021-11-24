53b12bd04e4d52caea5d719100d182bdf670b799
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-24 08:07:46 +0700

    login.php

---
+++ login.php
@@ -0,0 +1,33 @@
+
+<!-- Bootstrap CSS Tanpa Download -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" 
+    crossorigin="anonymous">
+<body>
+<div class="main">
+    <div style="padding : 100px;" >
+      <div align="center">
+        <h3 class=primary>SIGN IN</h3>
+        
+        <div class=container >
+            <form  method="post" action="">
+                <div class="mb-3">
+                    <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Username" style="width:401px; height: 47px; left:514px; top:323px"id="email">
+                </div>
+                <div class="mb-3">
+                    <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Password" style="width:401px; height: 47px; left:514px; top:323px"id="email">
+                </div>
+
+                <div class="button" style="margin-bottom: 0px" >
+                <button type="submit" class="btn btn-outline-dark" name ="login" style="background-color:white; width:401px; height: 47px; left:514px; bottom:20px;"><span style="color:black;">LOGIN</span></button>
+                </div>
+            </form>
+      </div>
+    </div> 
+</div>
+</body>
+<style>
+    body{
+        background-img: url('kostefilo/bglogin.svg');
+    }
+</style> 
