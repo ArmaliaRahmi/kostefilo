aac227ab1ba76ae49c955b7fe678868eb8a463eb
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-11-24 13:37:36 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,26 @@
-
 <!-- Bootstrap CSS Tanpa Download -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" 
     crossorigin="anonymous">
+<!DOCTYPE html>
+<html>
+<head>
+
+<style>
+::placeholder {
+  color: red;
+  opacity: 1; /* Firefox */
+}
+
+:-ms-input-placeholder { /* Internet Explorer 10-11 */
+ color: red;
+}
+
+::-ms-input-placeholder { /* Microsoft Edge */
+ color: red;
+}
+</style>
+</head>
 <body>
 <div
   class="bg-image d-flex justify-content-center align-items-center"
@@ -13,18 +31,18 @@
     <div class="main">
         <div style="padding : 100px;" >
         <div align="center">
-            <h2 class=primary style="color: white">SIGN IN</h2>
+            <h1 class=primary style="color: white"> LOGIN </h1>
             
             <div class=container >
                 <form  method="post" action="">
                     <div class="mt-5 mb-2">
-                        <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Username" style="width:401px; height: 47px; left:514px; top:323px"id="email">
+                        <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Username" style="background-color: transparent; width:401px; height: 47px; left:514px; top:323px">
                     </div>
                     <div class="mb-2">
-                        <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Password" style="width:401px; height: 47px; left:514px; top:323px"id="email">
+                        <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Password" style="background-color: transparent; width:401px; height: 47px; left:514px; top:323px">
                     </div>
                     <div class="button mt-5 py-5" >
-                    <button type="submit" class="btn btn-outline-dark" name ="login" style="background-color:white; width:401px; height: 47px; left:514px; bottom:20px;"><span style="color:black;">LOGIN</span></button>
+                    <button type="submit" class="btn btn-outline-dark" name ="login" style="background-color:white; width:401px; height: 47px; left:514px; bottom:20px; border-radius: 12px"><h4 style="color:37525A;">LOGIN</h4></button>
                     </div>
                 </form>
             </div>
@@ -32,3 +50,8 @@
         </div> 
     </div>
 </body>
+<style>
+    ::placeholder{
+  color: #FFFFFF
+  }
+</style>
