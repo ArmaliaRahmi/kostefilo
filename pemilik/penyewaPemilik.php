7aaf93e60ce09979477afeb9cebd78692728da19
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-12-17 09:15:21 +0700

    Update penyewaPemilik.php

--- pemilik/penyewaPemilik.php
+++ pemilik/penyewaPemilik.php
@@ -1,7 +1,8 @@
 <?php
-require 'sidebar.php';
+require 'sidebar3.php';
 require 'bootstrap.html';
 ?>
+
 <div class="content-container">
     <div class="container-fluid">
         <section class="ftco-section">
@@ -22,6 +23,7 @@ require 'bootstrap.html';
                                     <th class="text-center">Asal</th>
                                     <th class="text-center">WA</th>
                                     <th class="text-center">Lembaga</th>
+                                    <th class="text-center">Act</th>
                                 </tr>
                             </thead>
                             <tbody class="text-center">
@@ -31,6 +33,10 @@ require 'bootstrap.html';
                                     <td>Bogor</td>
                                     <td>0812-9876-4322</td>
                                     <td>UGM - Teknik Pertanian</td>
+                                    <td>
+                                    <a href="editdatapenyewapemilik.php" class="edit"><i class="far fa-edit" style="color: #5C8E9E" data-toggle="tooltip" title="Edit"></i></a>
+								    <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="far fa-trash-alt" style="color: #DE3B3B" data-toggle="tooltip" title="Delete"></i></a>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>02</td>
@@ -38,6 +44,10 @@ require 'bootstrap.html';
                                     <td>Solo</td>
                                     <td>0812-3445-2222</td>
                                     <td>UGM - Teknik Mesin</td>
+                                    <td>
+                                    <a href="editdatapenyewapemilik.php" class="edit"><i class="far fa-edit" style="color: #5C8E9E" data-toggle="tooltip" title="Edit"></i></a>
+								    <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="far fa-trash-alt" style="color: #DE3B3B" data-toggle="tooltip" title="Delete"></i></a>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>03</td>
@@ -45,6 +55,10 @@ require 'bootstrap.html';
                                     <td></td>
                                     <td></td>
                                     <td></td>
+                                    <td>
+                                    <a href="editdatapenyewapemilik.php" class="edit"><i class="far fa-edit" style="color: #5C8E9E" data-toggle="tooltip" title="Edit"></i></a>
+								    <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="far fa-trash-alt" style="color: #DE3B3B" data-toggle="tooltip" title="Delete"></i></a>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>04</td>
@@ -52,6 +66,10 @@ require 'bootstrap.html';
                                     <td>Bandung</td>
                                     <td>0878-3949-0898</td>
                                     <td>UNY - Teknik Elektro</td>
+                                    <td>
+                                    <a href="editdatapenyewapemilik.php" class="edit"><i class="far fa-edit" style="color: #5C8E9E" data-toggle="tooltip" title="Edit"></i></a>
+								    <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="far fa-trash-alt" style="color: #DE3B3B" data-toggle="tooltip" title="Delete"></i></a>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>05</td>
@@ -59,6 +77,10 @@ require 'bootstrap.html';
                                     <td></td>
                                     <td></td>
                                     <td></td>
+                                    <td>
+                                    <a href="editdatapenyewapemilik.php" class="edit"><i class="far fa-edit" style="color: #5C8E9E" data-toggle="tooltip" title="Edit"></i></a>
+								    <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="far fa-trash-alt" style="color: #DE3B3B" data-toggle="tooltip" title="Delete"></i></a>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>06</td>
@@ -66,53 +88,110 @@ require 'bootstrap.html';
                                     <td>Bandung</td>
                                     <td>0878-3949-0898</td>
                                     <td>UNY - Teknik Elektro</td>
+                                    <td>
+                                    <a href="editdatapenyewapemilik.php" class="edit"><i class="far fa-edit" style="color: #5C8E9E" data-toggle="tooltip" title="Edit"></i></a>
+								    <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="far fa-trash-alt" style="color: #DE3B3B" data-toggle="tooltip" title="Delete"></i></a>
+                                    </td>
                                 </tr>
                                 </tr>
                                 </div>
                             </tbody>
                         </table>
-                        <style>
-                             .table-curved {
-                            border-collapse: separate;
-                            border: solid black 2px;
-                            border-radius: 30px;
-                            border-left: 0px;
-                            border-top: 0px;
-                            }
-                            table.table-curved > thead > tr > th{
-                            border:2px solid black;
-                            }
-                            table.table-curved > tbody > tr > td{
-                                border:2px solid black;
-                            }
-                            .table-curved > thead:first-child > tr:first-child > th {
-                                border-bottom: 2px;
-                                border-top: solid black 2px;
-                            }
-                            .table-curved td, .table-curved th {
-                                border-left: 2px solid black;
-                                border-top: 2px solid black;
-                            }
-                            .table-curved > :first-child > :first-child > :first-child {
-                                border-top-left-radius: 30px;
-                            }
-                            .table-curved > :first-child > :first-child > :last-child {
-                                border-top-right-radius: 30px;
-                            }
-                            .table-curved > :last-child > :last-child > :first-child {
-                                border-bottom-left-radius: 30px;
-                            }
-                            .table-curved > :last-child > :last-child > :last-child {
-                                border-bottom-right-radius: 30px;
-                            }
-                            .content-container {
-                            padding-top: 50px;
-                            padding-left: 220px;
-                            }
-                        </style>
                     </div>
                 </div>
             </div>
+    <div id="deleteEmployeeModal" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<form>
+					<div class="modal-header">						
+						<h4 class="modal-title">Hapus Data Penyewa</h4>
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					</div>
+					<div class="modal-body">					
+						<p>Anda yakin ingin menghapus daya penyewa?</p>
+						<p class="text-warning"><small>This action cannot be undone.</small></p>
+					</div>
+					<div class="modal-footer">
+						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+						<input type="submit" class="btn btn-danger" value="Delete">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
         </section>
     </div>
 </div>
+
+<style>
+.table-curved {
+ border-collapse: separate;
+ border: solid black 2px;
+ border-radius: 30px;
+ border-left: 0px;
+ border-top: 0px;
+}
+table.table-curved > thead > tr > th{
+ border:2px solid black;
+}
+table.table-curved > tbody > tr > td{
+ border:2px solid black;
+}
+.table-curved > thead:first-child > tr:first-child > th {
+ border-bottom: 2px;
+ border-top: solid black 2px;
+}
+.table-curved td, .table-curved th {
+ border-left: 2px solid black;
+ border-top: 2px solid black;
+}
+.table-curved > :first-child > :first-child > :first-child {
+ border-top-left-radius: 30px;
+}
+.table-curved > :first-child > :first-child > :last-child {
+ border-top-right-radius: 30px;
+}
+.table-curved > :last-child > :last-child > :first-child {
+ border-bottom-left-radius: 30px;
+}
+.table-curved > :last-child > :last-child > :last-child {
+ border-bottom-right-radius: 30px;
+}
+.content-container {
+ padding-top: 50px;
+ padding-left: 220px;
+}
+/* Modal styles */
+.modal .modal-dialog {
+		max-width: 400px;
+	}
+	.modal .modal-header, .modal .modal-body, .modal .modal-footer {
+		padding: 20px 30px;
+	}
+	.modal .modal-content {
+		border-radius: 3px;
+	}
+	.modal .modal-footer {
+		background: white;
+		border-radius: 0 0 0 0;
+	}
+    .modal .modal-title {
+        display: inline-block;
+    }
+	.modal .form-control {
+		border-radius: 2px;
+		box-shadow: none;
+		border-color: #dddddd;
+	}
+	.modal textarea.form-control {
+		resize: vertical;
+	}
+	.modal .btn {
+		border-radius: 2px;
+		min-width: 100px;
+	}	
+	.modal form label {
+		font-weight: normal;
+	}	
+</style>
